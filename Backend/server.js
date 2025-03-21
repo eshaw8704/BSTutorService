fcff35cd77958a67d4c6bd675de3139c6f9b2606
Hitthenutz <matthewquan001@gmail.com>
2025-03-21 16:04:26 -0700

    w

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 const app = express();
+    app.get("/", (req,res) => 
+    {res.send("Server is ready  at http://localhost:5000")});
 
 app.listen(5000, ()  =>
 {
-    console.log("Server is ready at http://localhost:5000")
+    console.log("Server is ready at http://localhost:5000 hello")
 
 });
