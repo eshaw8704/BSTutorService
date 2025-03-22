e7efe23175fe1b1af3486848f0dd95b37edbe11b
Hitthenutz <matthewquan001@gmail.com>
2025-03-22 15:29:48 -0700

    BSTutor Name Change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1><BSTutors></BSTutors></h1>
+      <h1>BSTutors</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
