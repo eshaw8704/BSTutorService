23a9dc719b27c5037b514a332d09dd520cae3c94
marynolan6 <122495472+marynolan6@users.noreply.github.com>
2025-03-24 12:01:18 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
     app.get("/products",(req, res) => { });
 
 dotenv.config();
-console.log(process.env.MONGO_URI);
+//console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
