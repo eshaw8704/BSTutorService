a2a5f983c17a0780755a6a43a84812ba130f14aa
rumeethundal <rumeethundal03@gmail.com>
2025-03-26 15:15:21 -0700

    populated Appointment.js for Scrum 18

---
+++ backend/model/Appointment.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { User } from './models/User';
+
+const appointmentSchema = new mongoose.Schema({
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    tutor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+
+    subject: {
+        type: String,
+        required: true,
+    },
+
+    appointmentTime: {
+        type: Date,
+        required: true,
+    }
+});
+export const Appointment = mongoose.model("Appointment", appointmentSchema);
