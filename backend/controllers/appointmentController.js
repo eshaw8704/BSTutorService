1166174dd90c53c0dee02205da1c21436892ff8b
rumeethundal <rumeethundal03@gmail.com>
2025-03-29 17:25:36 -0700

    created time slots in model

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,34 +1,17 @@
-import { Appointment } from '../models/Appointment.js';
-
-// Get appointments for a specific student
-export const getAppointmentByStudent = async (req, res) => {
-    try {
-        const { studentID } = req.params;
-        const appointments = await Appointment.find({ student: studentID });
-
-        if (!appointments || appointments.length === 0) {
-            return res.status(404).json({ message: "No appointments found for this student." });
-        }
-
-        res.status(200).json(appointments);
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving appointments", error: error.message });
-    }
-};
+import Appointment from "../models/Appointment.js";
 
 export const createAppointment = async (req, res) => {
     try {
-        const { studentID, subject, appointmentTime, appointmentDay, tutor } = req.body;
+        const { subject, appointmentTime, tutor } = req.body;
 
-        if (!studentID || !subject || !appointmentTime || !appointmentDay || !tutor) {
+        if (!subject || !appointmentTime || !appointmentDate || !tutor) {
             return res.status(400).json({ message: "All fields are required." });
         }
 
         const newAppointment = new Appointment({
-            student: studentID,
             subject,
             appointmentTime,
-            appointmentDay,
+            appointmentDate,
             tutor,
         });
         await newAppointment.save();
--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -17,9 +17,14 @@ const appointmentSchema = new mongoose.Schema({
     subject: {
         type: String,
         required: true,
-        enum: ['Math', 'Science', 'English', 'History', 'Programming'], 
+        enum: ['Math', 'Science', 'English', 'History', 'Programming'] 
     },
     appointmentTime: {
+        type: String,
+        required: true,
+        enum: ["08:00 AM", "09:30 AM", "10:00 AM", "11:30 AM", "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM", "03:00 PM"]
+    },
+    appointmentDate: {
         type: Date,
         required: true,
     }
