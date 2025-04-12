c028eac2c4cae517243e3940cba89f043bb2d3f1
Hitthenutz <matthewquan001@gmail.com>
2025-04-12 00:38:14 -0700

    Merge Bug Fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,13 +14,3 @@ export default defineConfig({
     },
   },
 });
-
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-})
