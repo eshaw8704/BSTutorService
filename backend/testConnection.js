4e74da8ea21c569b347752757876da6c69b14ec4
liz3210 <161261874+liz3210@users.noreply.github.com>
2025-04-19 23:53:10 -0700

    Revert "connection"
    This reverts commit 09443e7dfcb2185c0624ee7312dc8153cf920f50.

--- backend/testConnection.js
+++ backend/testConnection.js
@@ -3,21 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
-export const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.error("❌ MongoDB connection error:", error.message);
-      process.exit(1); // Only exit on error
-    }
-  };
-  
-/*async function testConnection() {
+async function testConnection() {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
@@ -31,4 +17,4 @@ export const connectDB = async () => {
     }
 }
 
-testConnection();*/
+testConnection();
