09443e7dfcb2185c0624ee7312dc8153cf920f50
liz3210 <161261874+liz3210@users.noreply.github.com>
2025-04-19 23:04:06 -0700

    connection

--- backend/testConnection.js
+++ backend/testConnection.js
@@ -3,7 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function testConnection() {
+
+export const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error("❌ MongoDB connection error:", error.message);
+      process.exit(1); // Only exit on error
+    }
+  };
+  
+/*async function testConnection() {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
@@ -17,4 +31,4 @@ async function testConnection() {
     }
 }
 
-testConnection();
+testConnection();*/
