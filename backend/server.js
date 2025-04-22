44e6c98d4f8e6d811a5a6ae7484e977afb644dcd
rumeethundal <rumeethundal03@gmail.com>
2025-04-22 10:49:42 -0700

    frontend and backend run concurrently

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 // Port
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
